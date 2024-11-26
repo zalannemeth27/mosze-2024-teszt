24cd735e7a83d1b2d925f993d3b77820f7842c5c
zalannemeth27 <154541426+zalannemeth27@users.noreply.github.com>
2024-11-26 18:29:40 +0100

    Update main (2).cpp

--- main (2).cpp
+++ main (2).cpp
@@ -4,23 +4,22 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // N_ELEMENTS
-    std::cout << '1-100 ertekek duplazasa' // Pontosvessző hiánya
-    for (int i = 0;) //Hiányos ciklus
+    int *b = new int[N_ELEMENTS]; 
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+    for (int i = 1; i <= N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) //I-ig megy a ciklus
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:"  // ; hiányzik
+        std::cout << " Ertek: " << b[i] ;
     }    
-    std::cout << "Atlag szamitasa: " << std::endl;
+    std::cout << "Atlag szamitasa: " << std::endl;    
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++) // , helyett ;
+    for (int i = 0; i < N_ELEMENTS; i++)     
     {
-        atlag += b[i] //Hiányzik egy ;
-    } 
+        atlag += b[i] ; 
+    }
     atlag /= N_ELEMENTS;
-    std::cout << "Atlag: " << atlag << std::endl;
-    return 0;
-}
+    std::cout << "Atlag: " << atlag << std::endl;  
+return 0;
